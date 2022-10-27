85d30ad716393835a710bab46aced7e3b172f9b5
Tseegi_N <tnyamdorj@smith.edu>
2022-10-27 17:28:54 -0400

    Edits in House and Library

--- House.java
+++ House.java
@@ -1,12 +1,73 @@
-/* This is a stub for the House class */
-public class House {
+import java.util.ArrayList;
 
-  public House() {
+/* This is a stub for the House class */
+public class House extends Building {
+
+  private ArrayList<String> residents; 
+  private boolean hasDiningRoom;
+
+  public House(String name, String address, int nFloors, boolean hasDR) {
+    super(name, address, nFloors);
+    this.residents = new ArrayList<String>();
+    this.hasDiningRoom = hasDR;
+
     System.out.println("You have built a house: 🏠");
   }
 
+  public void moveIn(String name){
+    residents.add(name);
+    System.out.println(name + " is now a resident");
+  }
+
+  public String moveOut(String name){
+    if (residents.contains(name)){
+      residents.remove(name);
+      System.out.println(name + " has moved out from house");
+      return (name);
+    }
+    System.out.println("No resident with that name exists in this house.");
+    return("No resident with that name exists in this house.");
+  }
+
+  public boolean isResident(String person){
+    if (residents.contains(person)){
+      System.out.println(person + " is a resident");
+      return true;
+    }
+    System.out.println(person + " is not a resident");
+    return false;
+  }
+
+  public boolean hasDiningRoom(){
+    if (hasDiningRoom == true){
+      System.out.println("There's a dining hall at this house");
+    }
+    else{
+      System.out.println("No dining hall at this house");
+    }
+    return hasDiningRoom;
+  }
+  public boolean nResidents(){
+    if (residents.size() > 0){
+      System.out.println("Number of residents: " + residents.size());
+      return true;
+    }
+    return false;
+  }
+
   public static void main(String[] args) {
-    new House();
+    House newHouse = new House("Capen", "10 Elm Street", 4, false);
+    
+    System.out.println(newHouse);
+    newHouse.moveIn("Tseegi");
+    newHouse.moveIn("Nyamdorj");
+    newHouse.hasDiningRoom();
+    newHouse.nResidents();
+    newHouse.isResident("Nyamdorj");
+    newHouse.moveOut("Nyamdorj");
+    newHouse.isResident("Nyamdorj");
+    newHouse.nResidents();
+    
   }
 
 }
--- Library.java
+++ Library.java
@@ -1,12 +1,68 @@
-/* This is a stub for the Library class */
-public class Library {
+import java.util.Hashtable;
 
-    public Library() {
-      System.out.println("You have built a library: 📖");
-    }
-  
-    public static void main(String[] args) {
-      new Library();
-    }
-  
+/* This is a stub for the Library class */
+public class Library extends Building{
+
+  private Hashtable<String, Boolean> collection;
+  public Library(String name, String address, int nFloors) {
+    super(name, address, nFloors);
+
+    this.collection = new Hashtable<>();
+
+    System.out.println("You have built a library: 📖");
   }
+
+  public void addTitle(String title){
+    collection.put(title, true);
+    System.out.println(title + " is added to the library");
+  }
+  public String removeTitle(String title){
+    collection.remove(title);
+    System.out.println(title + " is removed from the library");
+    return (title);
+  }
+
+  public void checkOut(String title){
+    collection.replace(title, true, false);
+    System.out.println(title + " is checked out from the library");
+  }
+  public void Return(String title){
+    collection.replace(title, false, true);
+    System.out.println(title + " is returned to the library");
+  }
+
+  public boolean containsTitle(String title){
+    if (collection.containsKey(title)){
+      System.out.println(title + " is in the library collection");
+      return true;
+    }
+    System.out.println(title + " is not in the library collection");
+    return false;
+  }
+  public boolean isAvailable(String title){
+    if (collection.containsKey(title)){
+      if (collection.get(title) == true){
+        System.out.println(title + " is readily available");
+        return true;
+      }
+      System.out.println(title + " is not readily available");
+      return false;
+    }
+
+    throw new RuntimeException(title + " doesn't exist in the library. Check Collection!");
+  } 
+  public void printCollection(){
+    System.out.println(collection.toString());
+  } 
+  
+  public static void main(String[] args) {
+    Library Neilson = new Library("Neilson", "10 Elm Street", 5);
+    Neilson.addTitle("Brave New World by Aldous Huxley");
+    Neilson.addTitle("Maybe the Moon by Armistead Maupin");
+    Neilson.checkOut("Maybe the Moon by Armistead Maupin");
+    Neilson.containsTitle("Maybe the Moon by Armistead Maupin");
+    Neilson.isAvailable("Maybe the Moon by Armistead Maupin");
+    Neilson.printCollection();
+  }
+  
+}
